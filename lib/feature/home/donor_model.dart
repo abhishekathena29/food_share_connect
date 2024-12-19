2479f6e6a8a645411d3e3d8a609d2188db8b98dd
AnanyaKD <pratrao123@.com>
2024-12-19 10:26:56 +0530

    quantity

--- lib/feature/home/donor_model.dart
+++ lib/feature/home/donor_model.dart
@@ -5,13 +5,13 @@ class DonorModel {
   String
       foodName; // Includes food name and quantity in the format "Item (Quantity)"
   Timestamp addeddate;
-  bool isfulfilled;
+  // bool isfulfilled;
 
   DonorModel({
     required this.donorId,
     required this.foodName,
     required this.addeddate,
-    this.isfulfilled = false,
+    //this.isfulfilled = false,
   });
 
   Map<String, dynamic> toMap() {
@@ -19,15 +19,16 @@ class DonorModel {
       'donorId': donorId,
       'foodName': foodName,
       'addeddate': addeddate,
-      'isfulfilled': isfulfilled,
+      // 'isfulfilled': isfulfilled,
     };
   }
 
   factory DonorModel.fromMap(Map<String, dynamic> map) {
     return DonorModel(
-        donorId: map['donorId'] ?? '',
-        foodName: map['foodName'] ?? '',
-        addeddate: map['addeddate'],
-        isfulfilled: map['isfulfilled'] ?? false);
+      donorId: map['donorId'] ?? '',
+      foodName: map['foodName'] ?? '',
+      addeddate: map['addeddate'],
+      // isfulfilled: map['isfulfilled'] ?? false;
+    );
   }
 }
